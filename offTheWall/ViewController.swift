d6e983715e36f6c6add57872064741854d192d36
haneul-youn <skyoun0809@icloud.com>
2022-07-12 17:56:01 +0900

    Initial Commit

---
+++ offTheWall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  offTheWall
+//
+//  Created by SKY YOUN on 2022/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ offTheWallUITests/offTheWallUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  offTheWallUITestsLaunchTests.swift
+//  offTheWallUITests
+//
+//  Created by SKY YOUN on 2022/06/11.
+//
+
+import XCTest
+
+class offTheWallUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
